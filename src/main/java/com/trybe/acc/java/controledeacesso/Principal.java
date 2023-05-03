3e5c0eaf9facf5976db18ca527b0378520267927
caiobacode <caioodsf@gmail.com>
2023-05-03 17:58:38 -0300

    feat: finish system and show relatory

--- src/main/java/com/trybe/acc/java/controledeacesso/Principal.java
+++ src/main/java/com/trybe/acc/java/controledeacesso/Principal.java
@@ -1,5 +1,6 @@
 package com.trybe.acc.java.controledeacesso;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -40,10 +41,42 @@ public class Principal {
         System.out.println("Entre com uma opção válida");
       }
     }
-    
     scannerInstance.close();
     
-    System.out.print("Voce finalizou o sistema" + customers);
+    int totalMinors = 0;
+    int totalAdults = 0;
+    int totalElderlys = 0;
+    
+    for (int i = 0; i < customers.size(); i++) {
+      if (customers.get(i) < 18) {
+        totalMinors++;
+      } else if (customers.get(i) > 49) {
+        totalElderlys++;
+      } else {
+        totalAdults++;
+      }
+    }
+
+    float minorsFloat = totalMinors;
+    float adultsFloat = totalAdults;
+    float elderlysFloat = totalElderlys;
+    
+    DecimalFormat decimal = new DecimalFormat("0.00");
+    String minorsPercentage = decimal.format((minorsFloat * 100) / customers.size());
+    String adultsPercentage = decimal.format((adultsFloat * 100) / customers.size());
+    String elderlysPercentage = decimal.format((elderlysFloat * 100) / customers.size());
+    
+    System.out.println("----- Quantidade -----");
+    System.out.println("menores: " + totalMinors);
+    System.out.println("adultas: " + totalAdults);
+    System.out.println("a partir de 50: " + totalElderlys);
+    
+    System.out.println("");
+
+    System.out.println("----- Percentual -----");
+    System.out.println("menores: " + minorsPercentage + "%");
+    System.out.println("adultas: " + adultsPercentage + "%");
+    System.out.println("a partir de 50: " + elderlysPercentage + "%");
     
   }
 }
